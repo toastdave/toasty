19aba972d0ca7601c6b34d71335da9923c6bc7a2
DavidPierce24 <pie2ce@gmail.com>
2025-03-25 23:51:18 -0400

    add media query hook

--- src/components/custom/search.tsx
+++ src/components/custom/search.tsx
@@ -2,16 +2,19 @@
 
 import React, { useState, useRef, useEffect } from 'react'
 import { Input } from '../ui/input'
+import { useMediaQuery } from '@/hooks/use-media-query'
+import { SearchIcon } from 'lucide-react'
 
 const Search = () => {
   const [isFocused, setIsFocused] = useState(false)
+
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const isMobile = useMediaQuery("(max-width: 480px)");
 
 
   const handleFocus = () => {
     setIsFocused(true)
     console.log('Input focused')
-    
-   
   }
 
   const handleBlur = () => {
@@ -20,18 +23,19 @@ const Search = () => {
   }
 
   return (
-    <div className='flex-1 relative'>
-        <Input 
+    <div className='flex-1'>
+
+    {isMobile ? <SearchIcon className='w-6 h-6' /> : <Input 
           type="text" 
           placeholder="Search" 
           onFocus={handleFocus}
           onBlur={handleBlur}
           className={isFocused ? 'ring-2 ring-primary' : ''}
-        />
+        />}
 
         {isFocused && (
           <div 
-            className='fixed left-1/2 -translate-x-1/2 h-full w-1/2'
+            className='fixed left-1/2 -translate-x-1/2 h-80 w-1/2'
           >
             <div className='flex flex-col w-full h-full mt-4 bg-muted rounded-lg'>
                 
