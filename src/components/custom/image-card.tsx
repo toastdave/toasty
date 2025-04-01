1bb9543b56b7f120ac8e7e5cc7d476971ca2f475
DavidPierce24 <pie2ce@gmail.com>
2025-04-01 00:58:35 -0400

    update trending shows

--- src/components/custom/image-card.tsx
+++ src/components/custom/image-card.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const ImageCard = ({ imageUrl }: { imageUrl: string }) => {
   return (
     <div>
-        <Image src={`https://image.tmdb.org/t/p/w500${imageUrl}`} alt="Image Card" width={500} height={500} className='rounded w-6 h-12 object-cover outline-2 outline-gray-300' />
+        <Image src={`https://image.tmdb.org/t/p/w500${imageUrl}`} alt="Image Card" width={500} height={500} className='rounded min-w-24 min-h-36 object-cover' />
     </div>
   )
 }
--- src/components/custom/trending-shows.tsx
+++ src/components/custom/trending-shows.tsx
@@ -1,36 +1,42 @@
-import React from 'react'
-import ImageCard from './image-card'
+import React from "react";
+import ImageCard from "./image-card";
 
-export const revalidate = 3600 // invalidate every hour
+export const revalidate = 3600; // invalidate every hour
 
 const TrendingShows = async () => {
-    const trendingShows = await fetch(`https://api.themoviedb.org/3/trending/tv/week`, 
-        {
-            headers: {
-                Authorization: `Bearer ${process.env.TMDB_AUTH}`
-            }
-        }
-    )
+  const trendingShows = await fetch(
+    `https://api.themoviedb.org/3/trending/tv/week`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.TMDB_AUTH}`,
+      },
+    }
+  );
 
-    const data = await trendingShows.json()
+  const data = await trendingShows.json();
 
-    console.log(data)
+  console.log(data);
 
-
-  
   return (
-    <div className='flex flex-col gap-2'>
-        {data.results.map((show: any) => (
-            <div key={show.id} className='flex gap-4 bg-muted p-2 rounded-md'>
-                <ImageCard imageUrl={show.backdrop_path} />
-                <div className='flex flex-col gap-2'>
-                    <h2>{show.name}</h2>
-                    <p>{show.vote_average}</p>
-                </div>
-            </div>
-        ))}
+    <div className="flex gap-2 w-full overflow-x-auto">
+      {data.results.map((show: any, index: number) => (
+        <div
+          key={show.id}
+          className="flex gap-2 p-2 rounded-md w-full relative"
+        >
+          <div className="absolute top-0 left-0 w-6 h-12 bg-background/50 blur-sm"></div>
+          <p className="text-3xl font-bold absolute top-0 left-0">
+            {index + 1}
+          </p>
+          <ImageCard key={show.id} imageUrl={show.poster_path} />
+          {/* <div className='flex flex-col'>
+                    <h2 className='text-lg font-bold'>{show.name}</h2>
+                    <p className='text-sm text-muted-foreground'>{show.vote_average}</p>
+                </div> */}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default TrendingShows
+export default TrendingShows;
