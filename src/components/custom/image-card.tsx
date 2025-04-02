78067ec6b067d5a50388e5aaf6a4761b4280b94b
DavidPierce24 <pie2ce@gmail.com>
2025-04-01 23:57:47 -0400

    update image card sizing and position font

--- src/components/custom/image-card.tsx
+++ src/components/custom/image-card.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const ImageCard = ({ imageUrl }: { imageUrl: string }) => {
   return (
     <div>
-        <Image src={`https://image.tmdb.org/t/p/w500${imageUrl}`} alt="Image Card" width={500} height={500} className='rounded min-w-24 min-h-36 object-cover' />
+        <Image src={`https://image.tmdb.org/t/p/w500${imageUrl}`} alt="Image Card" width={500} height={500} className='rounded min-w-36 min-h-48 object-cover' />
     </div>
   )
 }
--- src/components/custom/trending-shows.tsx
+++ src/components/custom/trending-shows.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ImageCard from "./image-card";
+import { Lobster } from "next/font/google";
+
+// Initialize the font at module scope
+const lobsterFont = Lobster({ 
+    weight: '400',
+    subsets: ['latin']
+  });
 
 export const revalidate = 3600; // invalidate every hour
 
@@ -18,16 +25,17 @@ const TrendingShows = async () => {
   console.log(data);
 
   return (
-    <div className="flex gap-2 w-full overflow-x-auto">
+    <div className="flex gap-2 w-full overflow-x-auto no-scrollbar">
       {data.results.map((show: any, index: number) => (
         <div
           key={show.id}
           className="flex gap-2 p-2 rounded-md w-full relative"
         >
-          <div className="absolute top-0 left-0 w-6 h-12 bg-background/50 blur-sm"></div>
-          <p className="text-3xl font-bold absolute top-0 left-0">
-            {index + 1}
-          </p>
+          <div className="absolute top-0 left-0 rounded-full">
+            <p className={`text-[50px] font-bold ${lobsterFont.className}`}>
+              {index + 1}
+            </p>
+          </div>
           <ImageCard key={show.id} imageUrl={show.poster_path} />
           {/* <div className='flex flex-col'>
                     <h2 className='text-lg font-bold'>{show.name}</h2>
